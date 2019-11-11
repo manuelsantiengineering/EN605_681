7405aa786bf1a3076c8e618f728865a638d49093
Manny <manuelsantiengineering@gmail.com>
2019-11-10 19:07:23 -0500

    Started creating the DB connection

--- santiagoModule11/src/com/msanti16/servlet/services/FindReservationService.java
+++ santiagoModule11/src/com/msanti16/servlet/services/FindReservationService.java
@@ -1,10 +1,10 @@
 package com.msanti16.servlet.services;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.sql.SQLException;
 
+import com.msanti16.servlet.utils.ConvertDate;
 import com.msanti16.servlet.domain.Reservation;
 import com.msanti16.servlet.exceptions.BadIntegerParsingException;
 import com.msanti16.servlet.exceptions.BadUserNameException;
@@ -21,47 +21,8 @@ public class FindReservationService {
 	
 	public String findReservation(String startDate){
 		try{
-			
-    	if(startDate.chars().filter(ch -> ch == '/').count() != 2) {
-    		throw new BadIntegerParsingException("Incorrect date format", startDate);
-    	}
-    	
-    	String[] parsedDate = startDate.split("/");
-    	
-    	if(parsedDate.length != 3) {
-    		throw new BadIntegerParsingException("Incorrect date format", startDate);
-    	}
-      if(parsedDate[0].isEmpty()){
-          throw new BadIntegerParsingException("Reservation month can't be empty", parsedDate[0]);
-      }
-      if(parsedDate[1].isEmpty()){
-          throw new BadIntegerParsingException("Reservation day can't be empty", parsedDate[1]);
-      }
-      if(parsedDate[2].isEmpty()){
-          throw new BadIntegerParsingException("Reservation year can't be empty", parsedDate[2]);
-      }
-      if(parsedDate[0].length() > 2 || parsedDate[1].length() > 2 || parsedDate[2].length() != 4){
-      	throw new BadIntegerParsingException("Incorrect date format", startDate);
-      }
-        
-      int month, day, year;
-      try {
-	      month = Integer.parseInt(parsedDate[0]) - 1; // Jan = 0, Dec = 11
-	      day = Integer.parseInt(parsedDate[1]);
-	      year = Integer.parseInt(parsedDate[2]);
-      }catch (NumberFormatException exception){
-      	System.err.println("Error: " + exception.getMessage());
-        GenerateErrorHtml errorHtml = new GenerateErrorHtml(
-        		"<title>Beartooth Hiking Company</title>",
-        		"Make sure to use digits for the date values."
-        		);
-        errorHtml.generateHtml();
-        return errorHtml.toString();
-  		}
-      
-      Calendar calendarDate = Calendar.getInstance();
-      calendarDate.set(year, month, day, 0, 0);      
-      Date reservationDate = new Date(calendarDate.getTimeInMillis());
+			 
+      Date reservationDate = ConvertDate.convertStringFromClient(startDate);
       
       Reservation reservation = new Reservation();
       reservation.setFirst("TOMATERO");
---
+++ santiagoModule11/src/com/msanti16/servlet/utils/ConvertDate.java
@@ -0,0 +1,90 @@
+package com.msanti16.servlet.utils;
+
+import java.util.Date;
+
+import com.msanti16.servlet.exceptions.BadIntegerParsingException;
+
+import java.util.Calendar;
+
+public class ConvertDate {
+
+	public static Date convertStringFromDB(String strDate) 
+			throws BadIntegerParsingException {
+		
+		if(strDate.chars().filter(ch -> ch == '-').count() != 2) {
+  		throw new BadIntegerParsingException("Incorrect date format from DB", strDate);
+  	}
+  	
+  	String[] parsedDate = strDate.split("-");
+  	
+  	if(parsedDate.length != 3) {
+  		throw new BadIntegerParsingException("Incorrect date format from DB", strDate);
+  	}
+    if(parsedDate[0].isEmpty()){
+        throw new BadIntegerParsingException("Reservation year from DB can't be empty", parsedDate[0]);
+    }
+    if(parsedDate[1].isEmpty()){
+        throw new BadIntegerParsingException("Reservation month from DB can't be empty", parsedDate[1]);
+    }
+    if(parsedDate[2].isEmpty()){
+        throw new BadIntegerParsingException("Reservation day from DB can't be empty", parsedDate[2]);
+    }
+    if(parsedDate[0].length() > 2 || parsedDate[1].length() > 2 || parsedDate[2].length() != 4){
+    	throw new BadIntegerParsingException("Incorrect date format from DB", strDate);
+    }
+    
+    int month, day, year;
+    try {
+    	year = Integer.parseInt(parsedDate[0]);
+      month = Integer.parseInt(parsedDate[1]) - 1; // Jan = 0, Dec = 11
+      day = Integer.parseInt(parsedDate[2]);      
+    }catch (NumberFormatException exception){
+    	throw new BadIntegerParsingException("Date from DB is not using format yyyy-mm-dd", parsedDate[2]);
+		}
+    
+    Calendar calendarDate = Calendar.getInstance();
+    calendarDate.set(year, month, day, 0, 0);  
+		
+    return new Date(calendarDate.getTimeInMillis());
+	}
+	
+	public static Date convertStringFromClient(String strDate) 
+			throws BadIntegerParsingException {
+		
+		if(strDate.chars().filter(ch -> ch == '/').count() != 2) {
+  		throw new BadIntegerParsingException("Incorrect date format", strDate);
+  	}
+  	
+  	String[] parsedDate = strDate.split("/");
+  	
+  	if(parsedDate.length != 3) {
+  		throw new BadIntegerParsingException("Incorrect date format", strDate);
+  	}
+    if(parsedDate[0].isEmpty()){
+        throw new BadIntegerParsingException("Reservation month can't be empty", parsedDate[0]);
+    }
+    if(parsedDate[1].isEmpty()){
+        throw new BadIntegerParsingException("Reservation day can't be empty", parsedDate[1]);
+    }
+    if(parsedDate[2].isEmpty()){
+        throw new BadIntegerParsingException("Reservation year can't be empty", parsedDate[2]);
+    }
+    if(parsedDate[0].length() > 2 || parsedDate[1].length() > 2 || parsedDate[2].length() != 4){
+    	throw new BadIntegerParsingException("Incorrect date format", strDate);
+    }
+    
+    int month, day, year;
+    try {
+      month = Integer.parseInt(parsedDate[0]) - 1; // Jan = 0, Dec = 11
+      day = Integer.parseInt(parsedDate[1]);
+      year = Integer.parseInt(parsedDate[2]);
+    }catch (NumberFormatException exception){
+    	throw new BadIntegerParsingException("Make sure date uses format mm/dd/yyyy", parsedDate[2]);
+		}
+    
+    Calendar calendarDate = Calendar.getInstance();
+    calendarDate.set(year, month, day, 0, 0);  
+		
+    return new Date(calendarDate.getTimeInMillis());
+	}
+}
